b54813a7048cf58b8eb3c5cd346ece7b276ea061
AloneSmoker007 <brokenheart008899@gmail.com>
2026-09-18 21:56:55 +0500

    Register backup and restore routes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,6 @@
 import app from "./index.js";
 import { registerTask16Routes } from "./task16.routes.js";
+import { registerBackupRoutes } from "./backup.routes.js";
 
 registerTask16Routes(app);
+registerBackupRoutes(app);
