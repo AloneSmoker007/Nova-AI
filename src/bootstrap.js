6d8e51d86f8fba3680ed1a695b5ef630f288929b
AloneSmoker007 <brokenheart008899@gmail.com>
2026-09-18 21:53:51 +0500

    Phase 2: start tenant backup scheduler

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,6 @@
+import { startBackupScheduler } from "./services/backup.service.js";
 import app from "./index.js";
 import { registerTask16Routes } from "./task16.routes.js";
 
 registerTask16Routes(app);
+\nstartBackupScheduler();\n
