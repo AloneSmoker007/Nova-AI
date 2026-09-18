2fb797731008c0219fe7f9d6b348e8bc09670e80
AloneSmoker007 <brokenheart008899@gmail.com>
2026-09-18 17:47:11 +0500

    Fix automation database import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import compression from "compression";
 import rateLimit from "express-rate-limit";
 import Joi from "joi";
 
-import { checkDatabaseConnection, closeDatabaseConnection, isDatabaseConfigured } from "./config/database.js";
+import { checkDatabaseConnection, closeDatabaseConnection, isDatabaseConfigured, dbPool } from "./config/database.js";
 import { logger, createHttpLogger } from "./config/logger.js";
 import { runMigrations } from "./database/migrate.js";
 import { generateGeminiReply } from "./services/gemini.service.js";
